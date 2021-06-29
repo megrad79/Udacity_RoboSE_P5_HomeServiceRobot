b2a753683507fcf29ce945e2efc27c5ffe4cdcb6
Taryn Nicole <megrad.79@gmail.com>
2021-06-29 04:11:34 -0400

    Update pick_objects.sh

--- src/scripts/pick_objects.sh
+++ src/scripts/pick_objects.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 xterm -e " source /opt/ros/kinetic/setup.bash; roscore" &
 sleep 5
-xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/my_robot/worlds/taryn.world" &
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find my_robot)/world/taryn.world" &
 sleep 5
-xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/catkin_ws/src/my_robot/maps/map.yaml" &
+xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(rospack find my_robot)/map/map.yaml" &
 sleep 5
 xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
 xterm -e " rosrun pick_objects pick_objects"
-
-
