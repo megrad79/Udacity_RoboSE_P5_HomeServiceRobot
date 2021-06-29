f80c2fba9fb4a67e6f24915f6f22a99608d5a738
Taryn Nicole <megrad.79@gmail.com>
2021-06-29 04:10:09 -0400

    Update test_navigation.sh

--- src/scripts/test_navigation.sh
+++ src/scripts/test_navigation.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 xterm -e " source /opt/ros/kinetic/setup.bash; roscore" &
 sleep 5
-xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/my_robot/worlds/taryn.world" &
-sleep 2
-xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/catkin_ws/src/my_robot/maps/map.yaml" &
-sleep 2
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find my_robot)/world/taryn.world" &
+sleep 5
+xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(rospack find my_robot)/map/map.yaml" &
+sleep 5
 xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch"
-
