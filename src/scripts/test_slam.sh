bf6ba0c1726a57965c7c39000bb50881e5f34df6
Taryn Nicole <megrad.79@gmail.com>
2021-06-30 08:18:05 -0400

    Update test_slam.sh

--- src/scripts/test_slam.sh
+++ src/scripts/test_slam.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-xterm -e " source /opt/ros/kinetic/setup.bash; roscore" &
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find my_robot)/worlds/taryn.world" &
 sleep 5
-xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find my_robot)/world/taryn.world" &
-sleep 2
 xterm -e " roslaunch turtlebot_gazebo gmapping_demo.launch" &
-sleep 2
+sleep 5
 xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
-sleep 2
+sleep 5
 xterm -e " roslaunch turtlebot_teleop keyboard_teleop.launch"
