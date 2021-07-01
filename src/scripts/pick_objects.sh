16b375d56e3dd50c5458098e9a04b7c4c15e63d4
Taryn Nicole <megrad.79@gmail.com>
2021-07-01 10:25:17 +0000

    Add files via upload

--- src/scripts/pick_objects.sh
+++ src/scripts/pick_objects.sh
@@ -3,6 +3,6 @@ xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospa
 sleep 5
 xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(rospack find my_robot)/maps/map.yaml" &
 sleep 5
-xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch map_file:=$(rospack find my_robot)/maps/map.yaml" &
 sleep 5
 xterm -e " rosrun pick_objects pick_objects"
--- src/scripts/test_slam.sh
+++ src/scripts/test_slam.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find my_robot)/worlds/taryn.world" &
 sleep 5
-xterm -e " roslaunch turtlebot_gazebo gmapping_demo.launch" &
+xterm -e " roslaunch turtlebot_gazebo gmapping_demo.launch map_file:=$(rospack find my_robot)/maps/map.yaml" &
 sleep 5
-xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch map_file:=$(rospack find my_robot)/maps/map.yaml" &
 sleep 5
-xterm -e " roslaunch turtlebot_teleop keyboard_teleop.launch"
+xterm -e " roslaunch turtlebot_teleop keyboard_teleop.launch map_file:=$(rospack find my_robot)/maps/map.yaml"
