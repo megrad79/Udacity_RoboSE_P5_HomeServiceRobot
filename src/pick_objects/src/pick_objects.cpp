9871b08c8882b660a5b4f4b96a6156df9f4bfccc
Taryn Nicole <megrad.79@gmail.com>
2021-07-02 07:15:24 -0400

    updated goal 2 position to (1,1)

--- src/pick_objects/src/pick_objects.cpp
+++ src/pick_objects/src/pick_objects.cpp
@@ -48,8 +48,9 @@ int main(int argc, char** argv){
   goal2.target_pose.header.frame_id = "map";
   goal2.target_pose.header.stamp = ros::Time::now();
 
-  goal2.target_pose.pose.position.x = -1.0;
-  goal2.target_pose.pose.orientation.w = -1.0;
+  goal2.target_pose.pose.position.x = 1.0;
+  goal2.target_pose.pose.position.y = 1.0;
+  goal2.target_pose.pose.orientation.w = 1.0;
 
   ROS_INFO("Sending goal 2");
   ac.sendGoal(goal2);
