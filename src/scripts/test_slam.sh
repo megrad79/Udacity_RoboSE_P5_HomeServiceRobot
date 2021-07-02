626dc8243e6ef7fa62330d18cf835474bf27854b
Taryn Nicole <megrad.79@gmail.com>
2021-07-02 03:18:30 +0000

    Add files via upload

--- src/scripts/test_slam.sh
+++ src/scripts/test_slam.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find my_robot)/worlds/taryn.world" &
+xterm -e "export ROBOT_INITIAL_POSE='-x 3 -y -2 -Y 1.570796'; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find my_robot)/worlds/taryn.world" &
 sleep 5
-xterm -e " roslaunch turtlebot_gazebo gmapping_demo.launch map_file:=$(rospack find my_robot)/maps/map.yaml" &
+xterm -e "roslaunch turtlebot_gazebo gmapping_demo.launch map_file:=$(rospack find my_robot)/maps/map.yaml" &
 sleep 5
-xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch map_file:=$(rospack find my_robot)/maps/map.yaml" &
+xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
-xterm -e " roslaunch turtlebot_teleop keyboard_teleop.launch map_file:=$(rospack find my_robot)/maps/map.yaml"
+xterm -e "roslaunch turtlebot_teleop keyboard_teleop.launch"
