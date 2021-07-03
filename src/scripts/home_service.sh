5019ad57d846a3bb7cc8c01f8cc53a107ef2ec62
Taryn Nicole <megrad.79@gmail.com>
2021-07-02 22:53:26 -0400

    changing spawn location

--- src/scripts/home_service.sh
+++ src/scripts/home_service.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-xterm -e "export ROBOT_INITIAL_POSE='-x 3 -y -2 -Y 1.570796'; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find my_robot)/worlds/taryn.world" &
+xterm -e "export ROBOT_INITIAL_POSE='-x -3 -y -2 -Y 1.570796'; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find my_robot)/worlds/taryn.world" &
 sleep 5
 xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(rospack find my_robot)/maps/map.yaml" &
 sleep 5
